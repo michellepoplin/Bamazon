899aa4db47660179c811056f2a7c58c61fcccba4
Michelle Poplin <mlip6547@gmail.com>
2019-03-21 01:32:32 -0400

    tweaked the bamazon.js file for const connections

--- bamazon.js
+++ bamazon.js
@@ -9,8 +9,8 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
-    database: "Bamazon"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 });
 
 function init() {
